c49f0504ec0e4d95dcab0b871619bd7841f4d7d3
Andrew Grant <andrew.grant@mail.rit.edu>
2022-06-15 01:06:18 -0400

    make the armor bar display fractional values
    This works, but does not look super nice with >20 armor points

---
+++ src/main/java/io/github/andrew6rant/precise_armor/ArmorRenderer.java
@@ -0,0 +1,69 @@
+package io.github.andrew6rant.precise_armor;
+
+import com.mojang.blaze3d.systems.RenderSystem;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.DrawableHelper;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.attribute.EntityAttributes;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.math.MathHelper;
+
+public class ArmorRenderer extends DrawableHelper {
+    public static final ArmorRenderer INSTANCE = new ArmorRenderer();
+    public static final Identifier ARMOR_ICONS = new Identifier("precise_armor", "textures/gui/precise_armor.png");
+
+    public void init(MatrixStack matrices, PlayerEntity player) {
+        double armor = player.getAttributes().getValue(EntityAttributes.GENERIC_ARMOR);
+        int armorLevel = (int) Math.floor(armor);
+        MinecraftClient minecraft = MinecraftClient.getInstance();
+        int scaledWidth = minecraft.getWindow().getScaledWidth();
+        int scaledHeight = minecraft.getWindow().getScaledHeight();
+
+        // calculate armor offset variables (modified from InGameHud.class)
+        int j = MathHelper.ceil(player.getHealth());
+        int p = MathHelper.ceil(player.getAbsorptionAmount());
+        float f = Math.max((float)player.getAttributeValue(EntityAttributes.GENERIC_MAX_HEALTH), j);
+        int m = scaledWidth / 2 - 91;
+        int o = scaledHeight - 39;
+        int q = MathHelper.ceil((f + (float)p) / 2.0F / 10.0F);
+        int r = Math.max(10 - (q - 2), 3);
+
+        int x;
+        int y = o - (q - 1) * r - 10;
+
+        if (armor > 0) {
+            RenderSystem.setShaderTexture(0, ARMOR_ICONS);
+            for(int i = 0; i < armorLevel/2; ++i) {
+                x = m + i * 8;
+                drawTexture(matrices, x, y, 20, 20, 9, 9, 32, 32);
+            }
+            double armorRemainder = armor%2;
+
+            // doubles don't work with switch statements :(
+            if (armorRemainder > 0 && armorRemainder < 0.29) {
+                drawTexture(matrices, m + ((armorLevel/2) * 8), y, 10, 0, 9, 9, 32, 32);
+            } else if (armorRemainder > 0.29 && armorRemainder < 0.57) {
+                drawTexture(matrices, m + ((armorLevel/2) * 8), y, 20, 0, 9, 9, 32, 32);
+            } else if (armorRemainder > 0.57 && armorRemainder < 0.86) {
+                drawTexture(matrices, m + ((armorLevel/2) * 8), y, 0, 10, 9, 9, 32, 32);
+            } else if (armorRemainder > 0.86 && armorRemainder < 1.14) {
+                drawTexture(matrices, m + ((armorLevel/2) * 8), y, 10, 10, 9, 9, 32, 32);
+            } else if (armorRemainder > 1.14 && armorRemainder < 1.43) {
+                drawTexture(matrices, m + ((armorLevel/2) * 8), y, 20, 10, 9, 9, 32, 32);
+            } else if (armorRemainder > 1.43 && armorRemainder < 1.71) {
+                drawTexture(matrices, m + ((armorLevel/2) * 8), y, 0, 20, 9, 9, 32, 32);
+            } else if (armorRemainder > 1.71 && armorRemainder < 2) {
+                drawTexture(matrices, m + ((armorLevel/2) * 8), y, 10, 20, 9, 9, 32, 32);
+            } else {
+                if (armor != 20) { // if armor is 20, don't draw the last icon (this is temporary until I get >20 armor offsets working)
+                    drawTexture(matrices, m + ((armorLevel/2) * 8), y, 0, 0, 9, 9, 32, 32);
+                }
+            }
+            for(int i = 1; i < 10 - (armorLevel/2); ++i) {
+                x = m + ((i + armorLevel/2) * 8);
+                drawTexture(matrices, x, y, 0, 0, 9, 9, 32, 32);
+            }
+        }
+    }
+}
---
+++ src/main/java/io/github/andrew6rant/precise_armor/mixin/client/InGameHudMixin.java
@@ -0,0 +1,29 @@
+package io.github.andrew6rant.precise_armor.mixin.client;
+
+import io.github.andrew6rant.precise_armor.ArmorRenderer;
+import net.minecraft.client.gui.hud.InGameHud;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.player.PlayerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(InGameHud.class)
+public abstract class InGameHudMixin {
+    @Shadow
+    protected abstract PlayerEntity getCameraPlayer();
+
+    // Remove Vanilla's armor bar
+    @Redirect(method = "renderStatusBars", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;getArmor()I"))
+    private int dontRenderStatusBars(PlayerEntity instance) {
+        return 0;
+    }
+    // Render precise armor bar
+    @Inject(method = "renderStatusBars", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/profiler/Profiler;pop()V"))
+    private void newArmorBars(MatrixStack matrices, CallbackInfo ci) {
+        ArmorRenderer.INSTANCE.init(matrices, getCameraPlayer());
+    }
+}
